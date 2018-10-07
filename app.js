d5b52340b4ec4197e9c1e682b65bf4cba12d403b
Yusra Syed <yusra.april.23@gmail.com>
2018-10-07 05:25:08 +0500

    now gone

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const logger = require('morgan');
+// const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const brain = require('brain.js');
@@ -12,7 +12,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(cookieParser());
@@ -59,8 +59,6 @@ app.post('/', function(req, res){
 });
 
 
-
-server.listen(3000);
 app.listen(3000);
 console.log('server started on 3000');
 module.exports = app;
